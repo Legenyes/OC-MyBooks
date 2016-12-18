e73b62051b00bd719cb342a2b25fe5871fe1c4d6
Lévêque Sébastien <legenyes@gmail.com>
2016-12-18 16:28:42 +0100

    osef

--- app/routes.php
+++ app/routes.php
@@ -15,6 +15,5 @@ $app->get('/', function () use ($app) {
 // Book details with author
 $app->get('/book/{id}', function ($id) use ($app) {
     $book = $app['dao.book']->find($id);
-    $author = $app['dao.author']->find($id);
     return $app['twig']->render('book.html.twig', array('book' => $book, 'author' => $author));
 })->bind('book');;
